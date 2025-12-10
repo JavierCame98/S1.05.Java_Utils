2a775e5314bf47367af63a13528bbef6215fa3bf
Javier Castillo <javiercame98@gmail.com>
2025-12-10 16:20:00 +0100

    feat: creating a method to create a directory

--- src/main/java/ListAlphabeticallyTheDirectory/DirectoryAlphabetically.java
+++ src/main/java/ListAlphabeticallyTheDirectory/DirectoryAlphabetically.java
@@ -1,5 +1,29 @@
 package ListAlphabeticallyTheDirectory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 public class DirectoryAlphabetically {
+
+    public boolean createDirectory(String directoryName) throws IOException {
+        Objects.requireNonNull(directoryName, "directoryName cannot be null");
+        Path path = Paths.get(directoryName);
+        if (Files.exists(path)) {
+            if (Files.isDirectory(path)) {
+                System.out.println("Directory already exists: " + path.toAbsolutePath());
+                return true;
+            } else {
+                throw new IOException("A non-directory file exists at: " + path);
+            }
+        }
+        Files.createDirectories(path);
+        System.out.println("Directory created: " + path.toAbsolutePath());
+        return true;
+    }
+
+
 
 }
